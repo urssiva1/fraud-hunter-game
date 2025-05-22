e027b9db983100451a8a4460dd4e1926741922a0
urssiva1 <126922721+urssiva1@users.noreply.github.com>
2025-05-22 17:56:17 +1000

    Create fraud_hunter_game.py

---
+++ fraud_hunter_game.py
@@ -0,0 +1,6 @@
+git init
+git add fraud_hunter_game.py
+git commit -m "Initial commit with game logic"
+git branch -M main
+git remote add origin https://github.com/urssiva1/fraud-hunter-game.git 
+git push -u origin main
